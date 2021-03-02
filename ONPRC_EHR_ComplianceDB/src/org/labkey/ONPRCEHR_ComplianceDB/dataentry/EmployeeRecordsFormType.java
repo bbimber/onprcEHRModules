922f75850eb91a9b9e557d8765a02e82dda61128
labkey-jeckels <jeckels@labkey.com>
2021-03-01 18:18:03 -0800

    Merge ONPRC_EHR_ComplianceDB Java code to 20.11 conventions

---
+++ ONPRC_EHR_ComplianceDB/src/org/labkey/ONPRCEHR_ComplianceDB/dataentry/EmployeeRecordsFormType.java
@@ -0,0 +1,58 @@
+
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.ONPRCEHR_ComplianceDB.dataentry;
+
+import org.labkey.api.ehr.dataentry.FormSection;
+import org.labkey.api.ehr.dataentry.TaskFormSection;
+import org.labkey.api.ehr.dataentry.TaskForm;
+import org.labkey.api.ehr.dataentry.DataEntryFormContext;
+import org.labkey.api.module.Module;
+import org.labkey.api.view.template.ClientDependency;
+import org.labkey.ONPRCEHR_ComplianceDB.security.ONPRC_ComplianceDBEntryPermission;
+import java.util.Arrays;
+
+
+
+public class EmployeeRecordsFormType extends  TaskForm
+{
+    public static final String NAME = "employeerecords";
+
+    public EmployeeRecordsFormType(DataEntryFormContext ctx, Module owner)
+    {
+        super(ctx, owner, NAME, "Employee Records", "Employee Training Records", Arrays.asList(
+                new TaskFormSection(),
+                new EmployeeRequirementsFormSection()
+        ));
+
+        addClientDependency(ClientDependency.supplierFromPath("EHR_ComplianceDB/model/sources/EmployeeRecords.js"));
+        for  (FormSection s : getFormSections())
+        {
+            s.addConfigSource("EmployeeRecords");
+        }
+
+    }
+    @Override
+    protected boolean canInsert()
+    {
+        if (!getCtx().getContainer().hasPermission(getCtx().getUser(), ONPRC_ComplianceDBEntryPermission.class))
+            return false;
+
+        return super.canInsert();
+    }
+}
+
